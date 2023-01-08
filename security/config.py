fe6d21f1a230cf6e58c6a0705be4af8aa08ec7b7
kirill221100 <kirulkacool@gmail.com>
2023-01-08 10:18:25 +0700

    new register/auth system

---
+++ security/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = 'secret'
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
---
+++ security/jwt.py
@@ -0,0 +1,23 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from fastapi import HTTPException
+from security.config import Config as cfg
+
+
+def create_token(data: dict):
+    to_encode = data.copy()
+    exp = datetime.now() + timedelta(minutes=cfg.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp': exp})
+    encoded_jwt = jwt.encode(to_encode, cfg.SECRET_KEY, algorithm=cfg.ALGORITHM)
+    return encoded_jwt
+
+
+def verify_token(token: str):
+    try:
+        decoded_token = jwt.decode(token, cfg.SECRET_KEY, algorithms=[cfg.ALGORITHM])
+        login = decoded_token.get('login')
+        if login:
+            return login
+        raise HTTPException(status_code=401, detail='Invalid token')
+    except JWTError:
+        raise HTTPException(status_code=401, detail='Invalid token')
